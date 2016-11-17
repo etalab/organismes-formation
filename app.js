bf9dc789090f6a80972e035e870e515cae8f186e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-11-17 20:21:19 +0100

    Remove arrow functions and template strings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getData(url) {
   return fetch(url)
-    .then(response => response.json())
-    .catch((err) => {
+    .then(function (response) { return response.json() })
+    .catch(function (err) {
       console.error(err)
       throw err
   })
@@ -11,26 +11,26 @@ const url = 'https://inspire.data.gouv.fr/api/geogw/records'
 const form = document.getElementById('form')
 const result = document.getElementById('result')
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function (event) {
     event.preventDefault()
     result.innerHTML = '<div class="ui active centered inline massive loader"></div>'
     const q = document.getElementById('textinput').value
-    const promise = getData(url + `?q=${q}`)
+    const promise = getData(url + '?q=' + q)
     promise
-      .then( r => {
+      .then( function (r) {
         result.innerHTML = null
         if (!r.results.length) return result.insertAdjacentHTML('afterbegin', '<div>Aucune formation trouvée</div>')
 
-        r.results.map( data =>
-          result.insertAdjacentHTML('beforeend', `
-            <div class="formations">
-              <h3>${data.metadata.title}</h3>
-              <div>${data.metadata.description}</div>
-              <div><div class="formations-id">${data.recordId}</div></div>
-            </div>`)
-          )
+        r.results.map( function (data) {
+          return result.insertAdjacentHTML('beforeend',
+            '<div class="formations">' +
+              '<h3>' + data.metadata.title + '</h3>' +
+              '<div>' + data.metadata.description + '</div>' +
+              '<div><div class="formations-id">' + data.recordId + '</div></div>' +
+            '</div>')
+        })
 
-        result.insertAdjacentHTML('afterbegin', `<div><b>${r.count}</b> formations trouvées</div>`)
+        result.insertAdjacentHTML('afterbegin', '<div><b>' + r.count + '</b> formations trouvées</div>')
         $('.formations').transition('vertical flip in')
       }
     )
