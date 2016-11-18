c1c114b3f369d1f8868b4daecf4af9ddd22e8731
tmerlier <tmerlier@student.42.fr>
2016-11-18 16:31:08 +0100

    Sort by score

--- app.js
+++ app.js
@@ -23,29 +23,33 @@ form.addEventListener('submit', function (event) {
     promise
       .then( function (r) {
         result.innerHTML = null
-        if (!r.results.length) return result.insertAdjacentHTML('afterbegin', '<div>Aucun établissements de formations trouvés</div>')
+        if (!r.results.length) return result.insertAdjacentHTML('afterbegin', '<div>Aucun établissement de formation trouvé</div>')
 
-        r.results.map( function (data) {
+        r.results.sort( function (a, b) {
+          return a.score < b.score
+        }).map( function (data) {
+          const organization = data.organization
+
           return result.insertAdjacentHTML('beforeend',
             '<div class="formations">' +
-              '<div>Raison Sociale : <b> ' + displayValue(data.da_raison_sociale) + '</b></div>' +
-              '<div>Nombre de formateurs : <b>' + displayValue(data.form_total) + '</b></div>' +
-              '<div>Numéro d\'établissement de la structure : <b>' + displayValue(data.da_no_etab) + '</b></div>' +
-              '<div>Numéro de SIREN de la structure : <b>' + displayValue(data.da_siren) + '</b></div>' +
-              '<div>Numéro de la Déclaration d\'Activité : <b>' + displayValue(data.numero_de_da) + '</b></div>' +
+              '<div>Raison Sociale : <b> ' + displayValue(organization.da_raison_sociale) + '</b></div>' +
+              '<div>Nombre de formateurs : <b>' + displayValue(organization.form_total) + '</b></div>' +
+              '<div>Numéro d\'établissement de la structure : <b>' + displayValue(organization.da_no_etab) + '</b></div>' +
+              '<div>Numéro de SIREN de la structure : <b>' + displayValue(organization.da_siren) + '</b></div>' +
+              '<div>Numéro de la Déclaration d\'Activité : <b>' + displayValue(organization.numero_de_da) + '</b></div>' +
               '<div>' +
                 '<div class="adr-type">Adresse postale</div>' +
-                '<div>Ville de l\'adresse postale : <b>' + displayValue(data.adr_ville_postale) + '</b></div>' +
-                '<div>Code postal de l\'adresse postale : <b>' + displayValue(data.adr_code_postal_postale) + '</b></div>' +
-                '<div>Voie de l\'adresse postale : <b>' + displayValue(data.adr_rue_postale) + '</b></div>' +
-                '<div>Complément de l\'adresse postale : <b>' + displayValue(data.adr_rue_complement_postale) + '</b></div>' +
+                '<div>Ville de l\'adresse postale : <b>' + displayValue(organization.adr_ville_postale) + '</b></div>' +
+                '<div>Code postal de l\'adresse postale : <b>' + displayValue(organization.adr_code_postal_postale) + '</b></div>' +
+                '<div>Voie de l\'adresse postale : <b>' + displayValue(organization.adr_rue_postale) + '</b></div>' +
+                '<div>Complément de l\'adresse postale : <b>' + displayValue(organization.adr_rue_complement_postale) + '</b></div>' +
               '</div>' +
               '<div>' +
                 '<div class="adr-type">Adresse physique</div>' +
-                '<div>Ville de l\'adresse physique : <b>' + displayValue(data.adr_ville_physique) + '</b></div>' +
-                '<div>Code postal de l\'adresse physique : <b>' + displayValue(data.adr_code_postal_physique) + '</b></div>' +
-                '<div>Voie de l\'adresse physique : <b>' + displayValue(data.adr_rue_physique) + '</b></div>' +
-                '<div>Complément de l\'adresse physique : <b>' + displayValue(data.adr_rue_complement_physique) + '</b></div>' +
+                '<div>Ville de l\'adresse physique : <b>' + displayValue(organization.adr_ville_physique) + '</b></div>' +
+                '<div>Code postal de l\'adresse physique : <b>' + displayValue(organization.adr_code_postal_physique) + '</b></div>' +
+                '<div>Voie de l\'adresse physique : <b>' + displayValue(organization.adr_rue_physique) + '</b></div>' +
+                '<div>Complément de l\'adresse physique : <b>' + displayValue(organization.adr_rue_complement_physique) + '</b></div>' +
               '</div>' +
             '</div>')
         })
