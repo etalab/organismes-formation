e5e1d4147ea8cade9d33a17865b0eae084adf5a3
Axel Haustant <noirbizarre@gmail.com>
2016-11-19 02:41:11 +0100

    A little bit for result styling

--- app.js
+++ app.js
@@ -4,54 +4,81 @@ function getData(url) {
     .catch(function (err) {
       console.error(err)
       throw err
-  })
+    })
 }
 
-function displayValue(data) {
-  return data || 'Non communiqué'
+function displayValue(data, q) {
+  return highlight(data, q) || 'Non communiqué'
 }
+
+function formatAdress(data, type) {
+  const parts = ['<h3>Adresse ' + type + '</h3>'];
+  if (data['adr_rue_complement_' + type]) {
+    parts.push('<div>' + data['adr_rue_complement_' + type] + '</div>')
+  }
+  if (data['adr_rue_' + type]) {
+    parts.push('<div>' + data['adr_rue_' + type] + '</div>')
+  }
+  parts.push('<div>')
+  if (data['adr_code_postal_' + type]) {
+    parts.push(data['adr_code_postal_' + type] + '&nbsp;')
+  }
+  parts.push(data['adr_ville_' + type] + '</div>')
+  return parts.join('')
+}
+
+function highlight(text, q) {
+    if (!text) return
+    return q ? text.replace(RegExp('(' + q + ')', 'iu'), '<mark>$1</mark>') : text
+}
+
+function formatResult(organization, q) {
+  return '<article class="item formation">' +
+            '<div class="content">' +
+              '<h2 class="header">' + highlight(organization.da_raison_sociale, q) + '</h2>' +
+                '<div class="description">' +
+                  '<div class="ui stackable three column grid">' +
+                    '<div class="column">' +
+                      '<div>SIREN : <b>' + displayValue(organization.da_siren, q) + '</b></div>' +
+                      '<div>Déclaration d\'Activité : <b>' + displayValue(organization.numero_de_da, q) + '</b></div>' +
+                      '<div>Numéro d\'établissement : <b>' + displayValue(organization.da_no_etab, q) + '</b></div>' +
+                      '<div>Nombre de formateurs : <b>' + displayValue(organization.form_total) + '</b></div>' +
+                    '</div>' +
+                    '<div class="column">' + formatAdress(organization, 'postale') + '</div>' +
+                    '<div class="column">' + formatAdress(organization, 'physique') + '</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>' +
+          '</article>'
+}
+
 
 const url = '/organizations/'
+const limit = 50
 const form = document.getElementById('form')
 const result = document.getElementById('result')
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
     result.innerHTML = '<div class="ui active centered inline massive loader"></div>'
     const q = document.getElementById('textinput').value
-    const promise = getData(url + '?q=' + q)
+    const promise = getData(url + '?q=' + q + '&limit=' + limit)
     promise
       .then( function (r) {
         result.innerHTML = null
         if (!r.results.length) return result.insertAdjacentHTML('afterbegin', '<div>Aucun établissement de formation trouvé</div>')
 
+        const container = document.createElement('div')
+        container.classList.add('ui', 'divided', 'items')
+        result.appendChild(container)
         r.results.map( function (organization) {
-          return result.insertAdjacentHTML('beforeend',
-            '<div class="formations">' +
-              '<div>Raison Sociale : <b> ' + displayValue(organization.da_raison_sociale) + '</b></div>' +
-              '<div>Nombre de formateurs : <b>' + displayValue(organization.form_total) + '</b></div>' +
-              '<div>Numéro d\'établissement de la structure : <b>' + displayValue(organization.da_no_etab) + '</b></div>' +
-              '<div>Numéro de SIREN de la structure : <b>' + displayValue(organization.da_siren) + '</b></div>' +
-              '<div>Numéro de la Déclaration d\'Activité : <b>' + displayValue(organization.numero_de_da) + '</b></div>' +
-              '<div>' +
-                '<div class="adr-type">Adresse postale</div>' +
-                '<div>Ville de l\'adresse postale : <b>' + displayValue(organization.adr_ville_postale) + '</b></div>' +
-                '<div>Code postal de l\'adresse postale : <b>' + displayValue(organization.adr_code_postal_postale) + '</b></div>' +
-                '<div>Voie de l\'adresse postale : <b>' + displayValue(organization.adr_rue_postale) + '</b></div>' +
-                '<div>Complément de l\'adresse postale : <b>' + displayValue(organization.adr_rue_complement_postale) + '</b></div>' +
-              '</div>' +
-              '<div>' +
-                '<div class="adr-type">Adresse physique</div>' +
-                '<div>Ville de l\'adresse physique : <b>' + displayValue(organization.adr_ville_physique) + '</b></div>' +
-                '<div>Code postal de l\'adresse physique : <b>' + displayValue(organization.adr_code_postal_physique) + '</b></div>' +
-                '<div>Voie de l\'adresse physique : <b>' + displayValue(organization.adr_rue_physique) + '</b></div>' +
-                '<div>Complément de l\'adresse physique : <b>' + displayValue(organization.adr_rue_complement_physique) + '</b></div>' +
-              '</div>' +
-            '</div>')
+          return container.insertAdjacentHTML('beforeend', formatResult(organization, q))
         })
 
         result.insertAdjacentHTML('afterbegin', '<div><b>' + r.total + '</b> établissements de formations trouvés</div>')
-        $('.formations').transition('vertical flip in')
+        $('.formation').transition('vertical flip in')
       }
     )
   })
